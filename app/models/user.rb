90b5c8e33891395bd4a14a8d93891dcd583b4403
Gunotham.solanki <gunotham.solanki@fih-foxconn.com>
2023-05-15 16:01:05 +0530

    new app

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    has_many :microposts
+    validates :name, length: {maximum: 25}, presence: true
+    validates :email, length: {maximum: 40}, presence: true
+end
